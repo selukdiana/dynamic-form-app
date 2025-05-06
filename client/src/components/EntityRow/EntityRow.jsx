c22fda6c42b49de535dd0e3dc108be8ec49eeb44
Dziyana.Sialiuk <dianaselyuk02@mail.ru>
2025-05-06 14:03:22 +0300

    feat: create EntityRow component

---
+++ client/src/components/EntityRow/EntityRow.jsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router";
+import styles from "./EntityRow.module.css";
+import { FaPen } from "react-icons/fa";
+
+export const EntityRow = ({ entity }) => {
+  const navigate = useNavigate();
+  const changeEntityRowClickHandler = (id) => {
+    navigate(`/entity?id=${id}`);
+  };
+  return (
+    <tr className={styles.row}>
+      <td>{entity.name}</td>
+      <td>{entity.surname}</td>
+      <td>{entity.email}</td>
+      <td>{entity.education}</td>
+      <td>{entity.location}</td>
+      <td>{entity.age}</td>
+      <td>{`${entity.code || ""} ${entity.number || ""}`}</td>
+      <td
+        onClick={() => changeEntityRowClickHandler(entity.id)}
+        style={{ width: "1%" }}
+      >
+        <FaPen className={styles.icon} />
+      </td>
+    </tr>
+  );
+};
