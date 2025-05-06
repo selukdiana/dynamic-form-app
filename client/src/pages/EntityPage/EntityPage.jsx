2cb7649e194102ccb1e8484d2481121ded69673f
Dziyana.Sialiuk <dianaselyuk02@mail.ru>
2025-05-06 14:03:57 +0300

    feat: create pages

--- client/src/pages/EntityPage/EntityPage.jsx
+++ client/src/pages/EntityPage/EntityPage.jsx
@@ -1,3 +1,15 @@
+import { useSearchParams } from "react-router";
+import { Entity } from "../../components/Entity";
 import styles from "./EntityPage.module.css";
 
-export const EntityPage = () => {};
+export const EntityPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  return (
+    <section>
+      <div className="container">
+        <Entity editId={id} />
+      </div>
+    </section>
+  );
+};
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,42 @@
 import styles from "./HomePage.module.css";
 import { useSelector } from "react-redux";
-import { Entity } from "../../components/Entity";
+import { EntityRow } from "../../components/EntityRow";
+import React from "react";
+import { useNavigate } from "react-router";
 
 export const HomePage = () => {
   const entities = useSelector((state) => state.entities.data);
+  const navigate = useNavigate();
+  const handleAddBtnClick = () => {
+    navigate("/entity");
+  };
   return (
-    <>
-      {entities.length && entities.map((entity) => <Entity entity={entity} />)}
-    </>
+    <section>
+      <div className="container">
+        <div className={styles.content}>
+          <button onClick={handleAddBtnClick} className={styles.btn}>
+            +
+          </button>
+          <table className={styles.table} cellSpacing="0">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Surname</th>
+                <th>Email</th>
+                <th>Education</th>
+                <th>Location</th>
+                <th>Age</th>
+                <th>Phone Number</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {entities.length &&
+                entities.map((entity) => <EntityRow entity={entity} />)}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
   );
 };
