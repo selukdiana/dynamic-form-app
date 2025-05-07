6644b4e667e198860e9e13542cc085e7a71a0c7e
Dziyana.Sialiuk <dianaselyuk02@mail.ru>
2025-05-07 16:05:42 +0300

    feat: add data dipatching

--- client/src/components/EntityRow/EntityRow.jsx
+++ client/src/components/EntityRow/EntityRow.jsx
@@ -1,23 +1,31 @@
 import { useNavigate } from "react-router";
 import styles from "./EntityRow.module.css";
 import { FaPen } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 export const EntityRow = ({ entity }) => {
   const navigate = useNavigate();
   const changeEntityRowClickHandler = (id) => {
     navigate(`/entity?id=${id}`);
   };
+  const { ages, educations, codes } = useSelector((state) => state.data);
+  const ageValue = ages.find((age) => age.id == entity.ageId)?.value;
+  const educationValue = educations.find(
+    (education) => education.id == entity.educationId
+  )?.value;
+  const codeValue = codes.find((code) => code.id == entity.phoneCodeId)?.value;
+
   return (
     <tr className={styles.row}>
       <td>{entity.name}</td>
       <td>{entity.surname}</td>
       <td>{entity.email}</td>
-      <td>{entity.education}</td>
+      <td>{educationValue}</td>
       <td>{entity.location}</td>
-      <td>{entity.age}</td>
+      <td>{ageValue}</td>
       <td>
         {entity.isPhoneNumber === "on"
-          ? `${entity.phoneNumber.code} ${entity.phoneNumber.number}`
+          ? `${codeValue} ${entity.phoneNumber}`
           : "-"}
       </td>
       <td
